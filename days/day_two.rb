70ebb644ab9cee9e9865087bac097bf377e3d05a
Rachael Wright-Munn <rachaellw5.1@gmail.com>
2021-12-05 17:21:32 +0000

    Part two of Day 2

--- days/day_two.rb
+++ days/day_two.rb
@@ -3,6 +3,21 @@ require_relative "utilities"
 FILENAME = "input/day_two.txt"
 DIRECTION_REGEX = /(?<dir>forward|down|up)\s(?<dist>\d+)/
 
+def advanced_move(position, direction)
+  parsed_direction = direction.match(DIRECTION_REGEX)
+  return position unless parsed_direction
+  case parsed_direction[:dir]
+  when "forward"
+    position[:horizontal] += parsed_direction[:dist].to_i
+    position[:depth] += parsed_direction[:dist].to_i * position[:aim]
+  when "down"
+    position[:aim] += parsed_direction[:dist].to_i
+  when "up"
+    position[:aim] -= parsed_direction[:dist].to_i
+  end
+  position
+end
+
 def move(position, direction)
   parsed_direction = direction.match(DIRECTION_REGEX)
   return position unless parsed_direction
@@ -22,10 +37,15 @@ def part_one
   location[:horizontal] * location[:depth]
 end
 
-def position(input)
-  location = { horizontal: 0, depth: 0 }
+def part_two
+  location = position(file_as_strings(FILENAME), :advanced_move)
+  location[:horizontal] * location[:depth]
+end
+
+def position(input, move_function = :move)
+  location = { horizontal: 0, depth: 0, aim: 0 }
   input.each do |direction|
-    location = move(location, direction)
+    location = send(move_function, location, direction)
   end
   location
 end
--- spec/day_two_spec.rb
+++ spec/day_two_spec.rb
@@ -1,11 +1,44 @@
 require "./days/day_two.rb"
 
 RSpec.describe "Day 2" do
-  describe ".position" do
-    subject { position(input) }
-    let(:input) { ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]}
+  describe ".advanced_move" do
+    subject { advanced_move(position, direction) }
+    let(:position) { { horizontal: 0, depth: 0, aim: 0 } }
+    let(:direction) { "forward 10" }
 
-    it { is_expected.to eq({ horizontal: 15, depth: 10 }) }
+    it { is_expected.to eq({ horizontal: 10, depth: 0, aim: 0 }) }
+
+    context "when direction is down" do
+      let(:direction) { "down 5" }
+
+      it { is_expected.to eq({ horizontal: 0, depth: 0, aim: 5 }) }
+    end
+
+    context "when direction is up" do
+      let(:direction) { "up 17" }
+
+      it { is_expected.to eq({ horizontal: 0, depth: 0, aim: -17 }) }
+    end
+
+    context "when direction is nil" do
+      let(:direction) { "" }
+
+      it { is_expected.to eq({ horizontal: 0, depth: 0, aim: 0 }) }
+    end
+  end
+
+  describe ".position" do
+    subject { position(input, move_function) }
+    let(:input) { ["forward 5", "down 5", "forward 8", "up 3", "down 8", "forward 2"]}
+    let(:move_function) { :move }
+
+    it { is_expected.to eq({ aim: 0, horizontal: 15, depth: 10 }) }
+
+    context "when part two" do
+      let(:move_function) { :advanced_move }
+
+      it { is_expected.to eq({ aim: 10, horizontal: 15, depth: 60 }) }
+    end
   end
 
   describe ".move" do
@@ -38,5 +71,11 @@ RSpec.describe "Day 2" do
     subject { part_one }
 
     it { is_expected.to eq(2120749) }
+  end
+
+  describe ".part_two" do
+    subject { part_two }
+
+    it { is_expected.to eq(2138382217) }
   end
 end
